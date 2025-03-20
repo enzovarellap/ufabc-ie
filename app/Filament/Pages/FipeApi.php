3b575e2cdce38305ff5ab04f366ea2665d1c222b
enzovarellap <enzovpastore@gmail.com>
2025-03-19 23:02:21 -0300

    dev: add SDK for FIPE API requests

--- app/Filament/Pages/FipeApi.php
+++ app/Filament/Pages/FipeApi.php
@@ -2,11 +2,10 @@
 
 namespace App\Filament\Pages;
 
+use DeividFortuna\Fipe\FipeCarros;
 use Filament\Actions\Action;
 use Filament\Forms\Components\Select;
 use Filament\Pages\Page;
-use Http;
-
 class FipeApi extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
@@ -16,8 +15,7 @@ class FipeApi extends Page
 
     protected function getHeaderActions(): array
     {
-        $brands = \Http::get('https://parallelum.com.br/fipe/api/v1/carros/marcas')->json();
-
+        $brands = FipeCarros::getMarcas();
         $brandOptions = [];
         foreach ($brands as $brand) {
             $brandOptions[$brand['codigo']] = $brand['nome'];
@@ -37,7 +35,7 @@ class FipeApi extends Page
                         ->options(function ($get) {
                             $brand = $get('brand');
                             if (!is_null($brand)) {
-                                $models = \Http::get("https://parallelum.com.br/fipe/api/v1/carros/marcas/$brand/modelos")->json()['modelos'];
+                                $models = FipeCarros::getModelos($brand)['modelos'];
                                 $modelOptions = [];
                                 foreach ($models as $model) {
                                     $modelOptions[$model['codigo']] = $model['nome'];
@@ -56,7 +54,7 @@ class FipeApi extends Page
                             $model = $get('model');
                             $brand = $get('brand');
                             if (!is_null($model)) {
-                                $years = \Http::get("https://parallelum.com.br/fipe/api/v1/carros/marcas/$brand/modelos/$model/anos")->json();
+                                $years = FipeCarros::getAnos($brand, $model);
                                 $yearOptions = [];
                                 foreach ($years as $year) {
                                     $yearOptions[$year['codigo']] = $year['nome'];
@@ -67,7 +65,7 @@ class FipeApi extends Page
                         })
 
                 ])->action(function($data){
-                    $this->car = Http::get("https://parallelum.com.br/fipe/api/v1/carros/marcas/{$data['brand']}/modelos/{$data['model']}/anos/{$data['year']}")->json();
+                    $this->car = FipeCarros::getVeiculo($data['brand'], $data['model'], $data['year']);
                 })
         ];
     }

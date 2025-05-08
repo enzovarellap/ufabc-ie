456c635fa7d7c5b14071a64d7c92776c1923b312
enzovarellap <enzovpastore@gmail.com>
2025-05-07 23:10:33 -0300

    chore: adjust sorting for Dashboard

--- app/Filament/Widgets/CarStats.php
+++ app/Filament/Widgets/CarStats.php
@@ -7,6 +7,8 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class CarStats extends BaseWidget
 {
+    protected static ?int $sort = 1;
+
     protected function getStats(): array
     {
         $car = auth()->user()->car->first();

c6944fd3416754706c5c2f772cf6d749667a3973
enzovarellap <enzovpastore@gmail.com>
2025-05-08 15:05:09 -0300

    chore: adjust primary color and add logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,8 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Blue,
+                'primary' => '#329db3',
+                'secondary' => '#2f445a',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -38,7 +39,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-            ])
+            ])->brandLogo(asset('storage/logo/logo_no_bg.png'))
+            ->brandLogoHeight('100px')
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,

debdd8ed53686df942f1c0beb0f99c5fcfb1ce44
enzovarellap <enzovpastore@gmail.com>
2025-05-08 00:41:09 -0300

    feat: add bulk and row actions to services

--- app/Filament/Widgets/CarServicesTableWidget.php
+++ app/Filament/Widgets/CarServicesTableWidget.php
@@ -2,38 +2,70 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Car;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Collection;
 
 class CarServicesTableWidget extends BaseWidget
 {
     protected static ?int $sort = 2;
-    protected int | string | array $columnSpan = 'full';
+    protected int|string|array $columnSpan = 'full';
 
     public function table(Table $table): Table
     {
         return $table
-            ->query(function () {
-                $car = auth()->user()->car->first();
-                if (is_null($car)) {
-                    return null;
-                }
-
-                return $car->services();
-            })
+            ->relationship(fn(): BelongsToMany => auth()->user()->car()->first()->services())
             ->heading('Serviços Necessários')
             ->bulkActions([
-                Tables\Actions\BulkAction::make('test')
-                    ->action(function ($records) {
-                        dd($records);
-                    })
+                Tables\Actions\BulkAction::make('concluir')
+                    ->icon('heroicon-o-check')
+                    ->color('success')
+                    ->action(function (Collection $records) {
+                        $car = auth()->user()->car->first();
+                        if (!$car) {
+                            return;
+                        }
+
+                        foreach ($records as $record) {
+                            $car->services()->updateExistingPivot($record->id, ['is_done' => true]);
+                        }
+                    })->deselectRecordsAfterCompletion()
             ])
+            ->checkIfRecordIsSelectableUsing(
+                fn(Model $record): bool => ! $record->is_done,
+            )
             ->actions([
-                Tables\Actions\Action::make('test')
-                ->action(function ($record) {
-                    dd($record);
-                })
+                Tables\Actions\Action::make('concluir')
+                    ->icon('heroicon-s-check')
+                    ->color('success')
+                    ->tooltip('Concluir')
+                    ->iconButton()
+                    ->action(function ($record) {
+                        $car = Car::find($record->car_id);
+                        if (!$car) {
+                            return;
+                        }
+
+                        $car->services()->updateExistingPivot($record->id, ['is_done' => true]);
+                    })->hidden(fn($record): bool => $record->is_done),
+
+                Tables\Actions\Action::make('reverter')
+                    ->icon('heroicon-s-x-circle')
+                    ->color('danger')
+                    ->tooltip('Voltar para pendente')
+                    ->iconButton()
+                    ->action(function ($record) {
+                        $car = Car::find($record->car_id);
+                        if (!$car) {
+                            return;
+                        }
+
+                        $car->services()->updateExistingPivot($record->id, ['is_done' => false]);
+                    })->hidden(fn($record): bool => !$record->is_done),
             ])
             ->columns([
                 Tables\Columns\TextColumn::make('name')
@@ -41,15 +73,17 @@ class CarServicesTableWidget extends BaseWidget
 
                 Tables\Columns\TextColumn::make('price')
                     ->label('Preço')
+                    ->alignCenter()
                     ->money('BRL'),
 
                 Tables\Columns\IconColumn::make('is_done')
                     ->boolean()
-                    ->label('Realizado?'),
+                    ->label('Pendente')
+                    ->alignCenter(),
 
             ])->filters([
                 Tables\Filters\TernaryFilter::make('is_done')
-                    ->label('Serviços Realizados')
+                    ->label('Serviços Pendentes')
                     ->placeholder('Todos')
                     ->default(false)
             ]);

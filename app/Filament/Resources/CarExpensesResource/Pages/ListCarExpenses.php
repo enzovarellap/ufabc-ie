a09d58a93824e5e37acdacdd47fd3f3213ee3851
enzovarellap <enzovpastore@gmail.com>
2025-05-08 17:02:35 -0300

    chore: cosmetic changes to create action for car expenses

--- app/Filament/Resources/CarExpensesResource/Pages/ListCarExpenses.php
+++ app/Filament/Resources/CarExpensesResource/Pages/ListCarExpenses.php
@@ -13,7 +13,10 @@ class ListCarExpenses extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->label('Nova Despesa')
+                ->icon('heroicon-s-plus')
+                ->color('info'),
         ];
     }
 }

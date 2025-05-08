27b36a5cfd5f69bb418bfc5f6f8a6bea86991ae5
enzovarellap <enzovpastore@gmail.com>
2025-05-08 15:52:04 -0300

    dev: add Filament Colors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentColor::register([
+            'indigo' => Color::Indigo,
+            'fuchsia' => Color::Fuchsia,
+            'slate' => Color::Slate,
+            'teal' => Color::Teal,
+            'lime' => Color::Lime,
+            'orange' => Color::Orange,
+            'violet' => Color::Violet,
+        ]);
     }
 }
